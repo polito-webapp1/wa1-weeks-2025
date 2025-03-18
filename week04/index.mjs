c68bb6d3677b81ce27669b188fa73ee2898b1594
Fulvio Corno <fulvio.corno@polito.it>
2025-03-18 23:04:52 +0100

    express example

--- week04/index.mjs
+++ week04/index.mjs
@@ -1,9 +1,29 @@
 import express from 'express'
+import morgan from 'morgan';
 
 const app = express() ;
 
+app.use(express.json())
+app.use(morgan('dev'))
+
 // Define routes and web pages
 app.get('/', (req, res) =>	res.send('Hello World!')) ;
+
+app.get('/user', (req, res) => {
+    let u = { name: 'Fulvio', id:123 }
+    res.json(u)
+})
+
+app.post('/user', (req, res) => {
+    console.log(req.body)
+    res.end()
+})
+
+app.get('/user/:id/name', (req, res) => {
+    const id = req.params.id 
+
+    res.json({"id": id, "name": "Tom"})
+})
 
 // Activate server
 app.listen(3000, () =>	console.log('Server	ready')) ;
